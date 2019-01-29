9c6eff0479536e1778172d1cfc2b03aee15e3bd1
Will K <wjk68@case.edu>
2019-01-28 22:04:11 -0500

    Working on article plotting

--- medium/bargraphs.py
+++ medium/bargraphs.py
@@ -6,17 +6,10 @@ import pandas as pd
 import math
 import time
 
-driver = webdriver.Chrome()
-driver.get("https://medium.com/me/stats")
-input('Waiting for you to log in. Press enter when ready: ')
-earliest_article_date = parser.parse(
-    input('Enter earliest article date as string: ')).date()
-days = (datetime.now().date()
-        - earliest_article_date).total_seconds() / (60 * 60 * 24)
-months = math.ceil(days / 30)
+from webdriver_manager.chrome import ChromeDriverManager
 
 
-def get_all_pages(driver, xpath, months):
+def get_all_pages(driver, xpath, months, suffix):
 
     # Initially starting at today
     latest_date_in_graph = datetime.now().date()
@@ -62,18 +55,49 @@ def get_all_pages(driver, xpath, months):
         # Go to the previous graph
         driver.find_element_by_xpath(xpath).click()
         time.sleep(2)
-        print(f'{100 * m /(months)}% complete.', end='\r')
+        print(f'{100 * m /(months):.0f}% complete.', end='\r')
 
     results = pd.DataFrame({'date': pd.to_datetime(
-        dates), 'views': views}).groupby('date').sum()
+        dates), suffix: views}).groupby('date').sum()
     results = results.loc[results[results['views'] != 0.0].index.min():, ]
     print('First views on ', str(results.index.min().date()))
+
+    # Save using the date as the file name
+    fname = f'data/{str(datetime.now().date())}_{suffix}'
+    results.to_parquet(fname)
+    print('Stats saved to ', fname)
+
     return results
 
 
-xpath = input('Paste xpath as string: ')
+if __name__ == "__main__":
+    # Go to the website
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.get("https://medium.com/me/stats")
+    # Wait for user to log in
+    input('Waiting for you to log in. Press enter when ready: ')
 
-results = get_all_pages(driver, xpath, months)
-fname = f'{str(datetime.now().date())}_stats'
-results.to_parquet(fname)
-print('Stats saved to ', fname)
+    # Find earliest date
+    earliest_article_date = parser.parse(
+        input('Enter earliest article date as string: ')).date()
+    days = (datetime.now().date()
+            - earliest_article_date).total_seconds() / (60 * 60 * 24)
+    months = math.ceil(days / 30)
+
+    # Get the xpath from user
+    xpath = input('Paste xpath with no quotation marks: ')
+    # Gather the results
+    results = get_all_pages(driver, xpath, months, suffix='views')
+    print('Refresh page and click on reads')
+
+    # Get the xpath from user
+    xpath = input('Paste xpath with no quotation marks: ')
+    # Gather the results
+    results = get_all_pages(driver, xpath, months, suffix='reads')
+
+    print('Refresh page and click on fans')
+    # Get the xpath from user
+    xpath = input('Paste xpath with no quotation marks: ')
+    # Gather the results
+    results = get_all_pages(driver, xpath, months, suffix='fans')
+    print("Complete. All results saved in data directory.")
